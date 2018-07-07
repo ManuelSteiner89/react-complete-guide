fa61740e1393beab80a9d7d8ce78535dd203fa7b
Manuel Steiner <manuel.steiner89@gmail.com>
2018-07-07 22:24:39 +0200

    upd
    just some updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
   render() {
@@ -7,6 +9,9 @@ class App extends Component {
     <div className="App">
       <h1> Hi, I'm a React App</h1>
       <p>This is really working!</p>
+      <Person name="Igor" age="99"/>
+      <Person name="Benis" age="69">My Hobbies: Rubbing</Person>
+      <Person name="Vegana" age="12"/>
     </div>
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
