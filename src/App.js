da891fd4fadcb7c860e14effd1694912c9345f63
Manuel Steiner <manuel.steiner89@gmail.com>
2018-07-07 23:28:15 +0200

    updt

--- src/App.js
+++ src/App.js
@@ -36,11 +36,22 @@ nameChangedHandler= (event) =>{
 )
 }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border  : '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
   return (
     <div className="App">
       <h1> Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name
+      <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name
       </button>
       <Person 
       name={this.state.persons[0].name} 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 
 //ES6 equivalent to function {}
 //props = properties
 const person = (props) =>{
     return (
-    <div>
+    <div className="Person">
         <p onClick={props.onClick}>I'm {props.name} and I am  {props.age} years old!</p>
         <p>{props.children}</p>  
         <input type="text" onChange={props.changed} value={props.name}/>  
